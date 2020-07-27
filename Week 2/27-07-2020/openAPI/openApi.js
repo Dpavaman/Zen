908f37e602aa18386cfeca155bd145c38d279814
Pavaman Dabeer <67939138+Dpavaman@users.noreply.github.com>
2020-07-27 22:58:10 +0530

    Delete openApi.js

--- Week 2/27-07-2020/openAPI/openApi.js
+++
@@ -1,7 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('GET', "https://dog.ceo/api/breeds/list/all", true);
-request.onload = function() {
-  var data = JSON.parse(this.response)
-  console.log(data)
-  }
-  request.send()
