24444bf42c45d69dd0b5c1e2e742cbfb7562d180
Pavaman Dabeer <67939138+Dpavaman@users.noreply.github.com>
2020-07-29 01:17:46 +0530

    Delete APIKey.js

--- Week 2/27-07-2020/Auth/APIKey.js
+++
@@ -1,7 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('GET', "https://api.openweathermap.org/data/2.5/weather?lat=12.9716&lon=77.5946&appid=f66e464f223acdf197a09268ca0ec92b", true);   // Lattitude and Longitude of Bangalore.
-request.onload = function() {
-  var data = JSON.parse(this.response)
-  console.log(data)
-  }
-  request.send()
